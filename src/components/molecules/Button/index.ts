e042419e1e491f639d4cac9d4cdca485dee50545
JonathanYbarra <jonatha.ybarra@gmail.com>
2022-07-31 19:39:21 -0300

    fix: fix ixport button

--- src/components/molecules/Button/index.ts
+++ src/components/molecules/Button/index.ts
@@ -1,34 +1,4 @@
 import { Button } from "./Button";
 
 export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
-export { Button };import { Button } from "./Button";
-
-export default Button;
 export { Button };
